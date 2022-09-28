63e797e1d0518ff0b197a93fe69fddca20408e23
yzhatca <yzharsenal@gmail.com>
2022-09-27 20:54:37 -0400

    update package.json for heroku

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ require("dotenv/config")
 const mongoString = process.env.DATABASE_URL */
 
 //端口
-app.listen(3000, () => {
+/* app.listen(3000, () => {
     console.log('server is on')
-})
+}) */
 
 mongoose.connect(process.env.DB_CONNECTION, {
     useUnifiedTopology: true,
@@ -32,6 +32,16 @@ app.use(cors())
 app.use(bodyParser.json())
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error', { title: 'Error'});
+  });
 
 // use res.render to load up an ejs view file
 
@@ -55,4 +65,6 @@ app.get('/services', (req, res) => {
     res.render('pages/services');
 });
 
-app.use('/posts', postRoute)
+app.use('/posts', postRoute);
+
+module.exports = app;
